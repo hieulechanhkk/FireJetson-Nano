1c6a1a91366eddf144dc0a316eded9ae7c87328e
doanhnhantre <hieulechanhkk@gmail.com>
2022-06-11 08:04:34 +0700

    update jetson

--- FireDetection/Contour.py
+++ FireDetection/Contour.py
@@ -2,9 +2,9 @@
 import numpy as np
 import cv2
 from imutils import resize
-from keras.models import load_model
-from keras.preprocessing.image import img_to_array
-from keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 
 import paho.mqtt.client as paho
 broker="broker.hivemq.com"
